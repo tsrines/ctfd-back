7209c84c1faf801c00d3343b72c7e223304c9487
Rines.io <tsrines@gmail.com>
2021-01-22 21:00:49 -0500

    params for toggle added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :sessions
 
   get '/upload', to: 'amazon_s3_uploads#set_s3_direct_post'
-  get '/toggle', to: 'users#toggle'
+  get '/toggle/:id', to: 'users#toggle'
   match '*path', to: 'sessions#not_found', via: %i[get post put delete]
 end
