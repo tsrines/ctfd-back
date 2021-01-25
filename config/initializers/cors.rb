e67068c19506d4c32d35e61a3fac3a035abcf113
Rines.io <tsrines@gmail.com>
2021-01-25 08:46:14 -0500

    added domain back

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
-    origins 'netlify.app', 'localhost:3001'
+    origins 'kind-neumann-eeb20b.netlify.app', 'localhost:3001'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
              credentials: true
   end
 end
+Rails.application.config.action_controller.forgery_protection_origin_check =
+  false
